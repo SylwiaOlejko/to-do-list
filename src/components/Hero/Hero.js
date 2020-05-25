75220f60ad54ed111cf61a16000cd7338c88b8c3
Bartosz Łepski <lepski.bartosz@gmail.com>
2020-05-25 18:48:33 +0200

    Implement Redux

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,16 +3,16 @@ import styles from './Hero.scss';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
 
-const Hero = ({ titleText, imageSrc }) => (
+const Hero = ({ titleText, image }) => (
   <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(titleText)}</h2>
-    <img className={styles.image} src={imageSrc}/>
+    <img className={styles.image} src={image}/>
   </header>
 );
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
-  imageSrc: PropTypes.string.isRequired,
-}
+  image: PropTypes.string,
+};
 
 export default Hero;
