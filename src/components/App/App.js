d3ae7c2cd7fcd090bd5300214ca81e78f196b247
Sylwia Olejko <sylwia.olejko@gmail.com>
2020-07-05 03:18:18 +0200

    04.07.2020.2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import styles from './App.scss';
 
-import List from './List.js';
+import list from './List.js';
+
 
 class App extends React.Component {
   render() {
     return (
       <div id="app">
       < main className = {styles.component}  >
-      
+      < List/>
         <h1 className = {styles.title}>My first React  </h1>
         <h2 className = {styles.subtitle}>Hello World!!!</h2>
-        <List  />
+        
       </main>
       
       </div>
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './App.scss';
+
+
+<header>
+< main className = {styles.component}  >
+    <h2 className = {styles.subtitle}> Things to do </h2>
+        <img src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'></img>
+</main>
+    
+
+</header>
