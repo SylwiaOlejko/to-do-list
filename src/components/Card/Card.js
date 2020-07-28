9a42ceb467cd43d48eab4b11868560f62d9af954
Sylwia Olejko <sylwia.olejko@gmail.com>
2020-07-28 19:46:27 +0200

    28.07.2020

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './Card.scss';
+
+class Card extends React.Component {
+
+    render() {
+        return (
+        <div className={styles.component}> {this.props.title}
+       
+        </div> 
+        
+        );
+    }
+
+}
+
+export default Card; 
