70cb9d3a026ed845925cf2782112c0bb62d7ecd4
Sylwia Olejko <sylwia.olejko@gmail.com>
2020-09-19 23:59:22 +0200

    19.09 03

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Container from '../Container/Container';
+import {NavLink, Link} from 'react-router-dom';
+import Hero from '../Hero/Hero';
+
+const FAQ = () => (
+  <Container>
+    <h2>Info</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <Link>
+      <NavLink exact to='/info' activeClassName='active'>Info
+      </NavLink>
+    </Link>
+    <Hero>
+      <h2>To jest bardzo groźna bakteria</h2>
+      <i name="fas fa-bacteria"></i>
+      <h3>Tak, ta bakteri jest groźna</h3>
+    </Hero>
+  </Container>
+);
+
+export default FAQ;
--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -82,6 +82,7 @@ const lists = [
   },
 ];
 
+
 const columns = [
   {
     id: 'column-1',
